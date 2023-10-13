a6aea0d082b2561626791daa8d0ce2541a117c91
ManuelCarrascal <tricdiart@gmail.com>
2023-10-12 21:03:40 -0500

    implementacion de spring validation.

--- src/main/java/com/procesos/inventario/model/User.java
+++ src/main/java/com/procesos/inventario/model/User.java
@@ -1,24 +1,40 @@
 package com.procesos.inventario.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
 @Entity
 @Data
-@AllArgsConstructor
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @NotNull(message = "firstName is required")
+    @Size(min= 1, max = 100,message = "firstName max 100 characters")
     private String firstName;
     private String lastName;
-    private String address;
+    @JsonIgnore
+    @OneToMany(mappedBy = "user")
+    private List<Address> address;
+    @NotNull(message = "email is required")
+    @Email
     private String email;
+    @NotNull(message = "phone is required")
+    @Size(min= 1, max = 16,message = "phone min 1 and max 16 characters")
     private String phone;
+    @NotNull(message = "password is required")
+    @Size(min= 8, max = 20,message = "password min 8 and max 20 characters")
     private String password;
+    @NotNull(message = "document is required")
+    @Size(min= 5, max = 20,message = "document min 5 and max 20 characters")
     private String document;
 
 }
